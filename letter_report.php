ad31d083fd803409a8543ed8b9dcaa31dbdcf3de
Philip Cali <philip.cali@gmail.com>
2011-11-30 15:31:17 -0600

    Breakdown of grades by letter, and rank

--- letter_report.php
+++ letter_report.php
@@ -27,12 +27,12 @@ require_once $CFG->dirroot.'/lib/gradelib.php';
 require_once $CFG->dirroot.'/grade/lib.php';
 require_once $CFG->dirroot.'/grade/report/grade_breakdown/lib.php';
 
-$courseid = required_param('id');
-$bound    = required_param('bound');
-$gradeid  = required_param('grade');
+$courseid = required_param('id', PARAM_INT);
+$bound    = required_param('bound', PARAM_FLOAT);
+$gradeid  = required_param('grade', PARAM_INT);
 $groupid  = optional_param('group', 0, PARAM_INT);
 
-if (!$course = get_record('course', 'id', $courseid)) {
+if (!$course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('nocourseid');
 }
 
@@ -46,45 +46,61 @@ require_capability('moodle/grade:viewall', $context);
 
 /// Build navigation
 $strgrades  = get_string('grades');
-$reportname = get_string('modulename', 'gradereport_grade_breakdown');
+$reportname = get_string('pluginname', 'gradereport_grade_breakdown');
 
-$navigation = grade_build_nav(__FILE__, $reportname, $courseid);
+$PAGE->set_url(new moodle_url('/grade/report/grade_breakdown/letter_report.php',
+    array(
+        'id' => $courseid,
+        'bound' => $bound,
+        'grade' => $gradeid,
+        'group' => $groupid
+    )
+));
 
-/// Print header
-print_header_simple($strgrades.': '.$reportname, ': '.$strgrades, $navigation,
-                    '', '', true, '', navmenu($course));
+$PAGE->set_context($context);
 
-/// Print the plugin selector at the top
-print_grade_plugin_selector($courseid, 'report', 'grade_breakdown');
+print_grade_page_head($course->id, 'report', 'grade_breakdown', $reportname, false);
 
 // This grade report has the functionality to print the right
 // group selector
-$gpr = new grade_plugin_return(array('type' => 'report', 'plugin' => 'grade_breakdown', 'courseid' => $courseid));
+$gpr = new grade_plugin_return(array(
+    'type' => 'report', 'plugin' => 'grade_breakdown', 'courseid' => $courseid
+));
 
-$grade_report = new grade_report_grade_breakdown($courseid, $gpr, $context, $gradeid, $groupid);
-$grade_report->pbarurl = $CFG->wwwroot.'/grade/report/grade_breakdown/letter_report.php?id='.
-                         $courseid . '&amp;bound='. $bound;
+$grade_report = new grade_report_grade_breakdown(
+    $courseid, $gpr, $context, $gradeid, $groupid
+);
+
+$grade_report->base_url = '/grade/report/grade_breakdown/letter_report.php?id='.
+    $courseid . '&amp;bound='. $bound;
+
 $grade_report->setup_groups();
 
 echo '<div class="selectors">'. $grade_report->group_selector. '</div>';
 
 // Get all the students in this course
 $roleids = explode(',', $CFG->gradebookroles);
-$graded_users = get_role_users($roleids, $context, 
+
+$graded_users = get_role_users($roleids, $context,
                 false, '', 'u.lastname ASC', true, $groupid);
+
 $userids = implode(',', array_keys($graded_users));
 
 $sql = "SELECT g.id, g.grademax, g.itemname, gc.fullname FROM
-            {$CFG->prefix}grade_items g,
-            {$CFG->prefix}grade_categories gc
-         WHERE g.id = {$gradeid}
-           AND (gc.id = g.categoryid 
+            {grade_items} g,
+            {grade_categories} gc
+         WHERE g.id = :gradeid
+           AND (gc.id = g.categoryid
             OR (gc.id = g.iteminstance AND g.categoryid IS NULL))
-           AND g.courseid = {$courseid}";
+           AND g.courseid = :courseid";
 
-$grade_item = get_record_sql($sql);
+$grade_item = $DB->get_record_sql($sql, array(
+    'gradeid' => $gradeid,
+    'courseid' => $courseid
+));
 
 $letters = grade_get_letters($context);
+
 $high = 100;
 foreach($letters as $boundary => $letter) {
     // Found it!
@@ -94,42 +110,51 @@ foreach($letters as $boundary => $letter) {
     $high = $boundary - (1 / (pow(10, 5)));
 }
 
-// In the event that we're looking at the max, students actually have the 
+// In the event that we're looking at the max, students actually have the
 // ability to go twice the max, so we must adhere to that rule
 $high = ($high == 100) ? $high * 2 : $high;
 
 $real_high = $grade_item->grademax * ($high / 100);
 $real_low  = $grade_item->grademax * ($bound / 100);
 
+$query_params = array(
+    'gradeid' => $gradeid,
+    'real_high' => $real_high,
+    'real_low' => $real_low
+);
+
 // Add group sql
 $group_select = "";
 $group_where  = "";
+
 if ($groupid) {
-    $group_select = ", {$CFG->prefix}groups_members gr ";
+    $query_params += array('groupid' => $groupid);
+
+    $group_select = ", {groups_members} gr ";
     $group_where = " AND u.id = gr.userid
-                     AND gr.groupid = {$groupid} ";
+                     AND gr.groupid = :groupid ";
 }
 
 // Get all the grades for the users within the range specified with $real_high and $real_low
-$sql = "SELECT u.id, g.id AS gradeid, g.finalgrade, u.firstname, u.lastname FROM 
-                {$CFG->prefix}grade_grades g,
-                {$CFG->prefix}user u
+$sql = "SELECT u.id, g.id AS gradeid, g.finalgrade, u.firstname, u.lastname FROM
+                {grade_grades} g,
+                {user} u
                 $group_select
             WHERE u.id = g.userid
               $group_where
-              AND g.itemid = {$gradeid}
+              AND g.itemid = :gradeid
               AND g.userid IN ({$userids})
-              AND g.finalgrade <= {$real_high}
-              AND g.finalgrade >= {$real_low}
+              AND g.finalgrade <= :real_high
+              AND g.finalgrade >= :real_low
             ORDER BY g.finalgrade DESC";
 
-$grades = get_records_sql($sql);
+$grades = $DB->get_records_sql($sql, $query_params);
 
 // No grades; tell them that, then die
 if (!$grades) {
-    print_heading(get_string('no_grades', 'gradereport_grade_breakdown'));
-    print_footer();
-    die();
+    echo $OUTPUT->heading(get_string('no_grades', 'gradereport_grade_breakdown'));
+    echo $OUTPUT->footer();
+    exit;
 }
 
 if ($grade_item->itemname) {
@@ -141,56 +166,53 @@ if ($grade_item->itemname) {
 }
 
 // Get the Moodley version of this grade item
-$grade_item = grade_item::fetch(array('id' => $gradeid));
-$groupname = ($groupid) ? ' in ' . get_field('groups', 'name', 'id', $groupid) : '';
+$item_params = array('id' => $gradeid);
+$grade_item = grade_item::fetch($item_params);
 
-print_heading(get_string('user_grades', 'gradereport_grade_breakdown') . 
-              $letters[$bound] . ' for ' . $name . $groupname);
+$g_params = array('id' => $groupid);
+$groupname = ($groupid) ? ' in ' . $DB->get_field('groups', 'name', $g_params) : '';
 
-$numusers = find_num_users($context, $groupid);
+echo $OUTPUT->heading(get_string('user_grades', 'gradereport_grade_breakdown') .
+    $letters[$bound] . ' for ' . $name . $groupname);
 
-// Use simple_tree is it exists
-$simple_tree = file_exists($CFG->dirroot . '/grade/edit/simple_tree');
+
+$numusers = count($graded_users);
 
 // Build the data
 $data = array();
 foreach ($grades as $userid => $gr) {
     $line = array();
-    $line[] = '<a href="'.$CFG->wwwroot.'/grade//report/user/index.php?id='.
-              $courseid.'&amp;userid='. $userid.'">' . fullname($gr). '</a>';
-    $line[] = grade_format_gradevalue($gr->finalgrade, $grade_item, true, 
-                                      GRADE_DISPLAY_TYPE_REAL);
-    $line[] = grade_format_gradevalue($gr->finalgrade, $grade_item, true, 
-                                      GRADE_DISPLAY_TYPE_PERCENTAGE);
+
+    $url = new moodle_url('/grade/report/user/index.php', array(
+        'id' => $courseid,
+        'userid' => $userid
+    ));
+
+    $line[] = html_writer::link($url, fullname($gr));
+
+    $line[] = grade_format_gradevalue($gr->finalgrade, $grade_item, true,
+        GRADE_DISPLAY_TYPE_REAL);
+
+    $line[] = grade_format_gradevalue($gr->finalgrade, $grade_item, true,
+        GRADE_DISPLAY_TYPE_PERCENTAGE);
+
     $line[] = find_rank($context, $grade_item, $gr, $groupid) . '/' . $numusers;
-    $line[] = print_edit_link($courseid, $gr->gradeid, $grade_item, $simple_tree);
+
+    $line[] = print_edit_link($courseid, $grade_item, $gr->gradeid);
     $data[] = $line;
 }
 
-$table = new object();
-$table->head = array(get_string('fullname'), 
-                     get_string('real_grade', 'gradereport_grade_breakdown'), 
-                     get_string('percent', 'grades'), 
-                     ($groupid ? get_string('group') : get_string('course')) . ' ' . get_string('rank', 'grades'), 
-                     get_string('edit', 'grades'));
+$table = new html_table();
+$table->head = array(
+    get_string('fullname'),
+    get_string('real_grade', 'gradereport_grade_breakdown'),
+    get_string('percent', 'grades'),
+    ($groupid ? get_string('group') : get_string('course')) . ' ' . get_string('rank', 'grades'),
+    get_string('edit', 'grades')
+);
+
 $table->data = $data;
 
-print_table($table);
-print_footer();
+echo html_writer::table($table);
+echo $OUTPUT->footer();
 
-function print_edit_link($courseid, $grade_gradeid, $grade_item, $simple_tree = false) {
-    global $CFG;
-
-    $tree = ($simple_tree) ? 'simple_tree' : 'tree';
-
-    if (!in_array($grade_item->itemtype, array('course', 'category'))) {
-        return '<a href="'.$CFG->wwwroot.'/grade/edit/'.$tree.'/grade.php?courseid='.
-                       $courseid.'&amp;id='.$grade_gradeid.'&amp;gpr_type=report'.
-                      '&amp;gpr_plugin=grade_breakdown&amp;gpr_courseid='.
-                       $courseid.'">'.get_string('edit', 'grades').'</a>';
-    } else {
-        return 'X';
-    }
-}
-
-?>
